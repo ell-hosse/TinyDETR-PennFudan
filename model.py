0f8f8e71ca2a37815410176a46d3e593561a41a1
Elaheh Hosseni <elahe.s.hs98@gmail.com>
2025-07-13 23:14:22 +0330

    Debug detr_resnet50 issue on torchvision ≥ 0.20.

--- model.py
+++ model.py
@@ -1,11 +1,16 @@
 import torch
-import torchvision
-from torchvision.models.detection import detr_resnet50
-from torchvision.models.detection.transform import GeneralizedRCNNTransform
+import torch.nn as nn
 
 def get_model(num_classes: int, pretrained: bool = True):
-    model = detr_resnet50(pretrained=pretrained, num_classes=num_classes)
-    return model
+    try:
+        from torchvision.models.detection import detr_resnet50
+        return detr_resnet50(weights=None, num_classes=num_classes)
+    except:
+        print("[!] DETR not available in torchvision ≥0.20 -> using torch.hub fallback")
+        model = torch.hub.load('facebookresearch/detr:main', 'detr_resnet50', pretrained=pretrained)
+        model.class_embed = nn.Linear(model.class_embed.in_features, num_classes)
+        return model
+
 
 def get_device(requested: str or None = None):
     if requested is None:
