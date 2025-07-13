92ed70c7c7562f1e65f8398269e400e6c397544e
Elaheh Hosseni <elahe.s.hs98@gmail.com>
2025-07-14 01:03:02 +0330

    Debug issue with shape mismatch header of hugging-face.

--- model.py
+++ model.py
@@ -1,16 +1,20 @@
+# model.py
 import torch
-from transformers import DetrForObjectDetection, DetrImageProcessor
+import torch.nn as nn
+from transformers import DetrForObjectDetection
 
 def get_model(num_classes=2, pretrained=True):
-    if pretrained:
-        model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50", num_labels=num_classes)
-    else:
-        model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50", ignore_mismatched_sizes=True)
-        model.class_labels_classifier = torch.nn.Linear(model.config.hidden_size, num_classes)
-    return model
+    model = DetrForObjectDetection.from_pretrained(
+        "facebook/detr-resnet-50",
+        num_labels=num_classes,
+        ignore_mismatched_sizes=True,
+    )
 
-def get_processor():
-    return DetrImageProcessor.from_pretrained("facebook/detr-resnet-50")
+    hid = model.class_labels_classifier.in_features
+    model.class_labels_classifier = nn.Linear(hid, num_classes)
+    model.bbox_predictor = nn.Linear(hid, 4)
+
+    return model
 
 def get_device(requested: str or None = None):
     if requested is None:
